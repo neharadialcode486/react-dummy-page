4c7c0a8b66dbfc467599f3812ff65bcbef467438
Neha Rai <neharadialcode@gmail.com>
2025-01-14 16:36:58 +0530

    spacing

--- src/components/ShareExperience.jsx
+++ src/components/ShareExperience.jsx
@@ -6,7 +6,7 @@ const ShareExperience = () => {
     <div>
       <div className="conatiner  flex items-center justify-between px-5 mx-auto max-w-[1240px] sm:py-10 pb-10 pt-5 lg:mt-5">
         <div className="bg-lightGray lg:ps-8  w-full rounded-2xl flex flex-col lg:flex-row gap-5 justify-between items-center lg:h-[250px] overflow-hidden">
-          <div className="w-full sm:py-7 py-5 sm:px-8 px-5">
+          <div className="w-full sm:py-7 py-5 lg:py-0 lg:px-0 sm:px-8 px-5">
             <Title
               title="500k"
               desc=" Hello world! Lorem ipsum dolor sit amet consectetur adipisicing elit. Hello world! Lorem ipsum dolor sit amet consectetur adipisicing elit."
@@ -15,7 +15,7 @@ const ShareExperience = () => {
               Invite Now
             </button>
           </div>
-          <div className="w-full flex sm:gap-3 gap-2 lg:rotate-6 xl:ps-10 h-[200px] overflow-hidden justify-end lg:justify-start">
+          <div className="w-full flex sm:gap-3 gap-2 lg:rotate-6 xl:ps-10 h-[200px] lg:h-auto overflow-hidden justify-end lg:justify-start">
             <div className="flex flex-col sm:gap-3 gap-2">
               <div className="sm:w-[150px] w-[100px] min-h-[80px] bg-gray sm:rounded-xl rounded-lg"></div>
               <div className="sm:w-[150px] w-[100px] min-h-[130px] bg-gray sm:rounded-xl rounded-lg"></div>
