b91967e8f57249fa170c389f377207d265b09df6
anil7020 <ak9565191@gmail.com>
2025-01-16 20:45:11 +0530

    extra route remove

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
 import Post from "./pages/Post";
-import ProductDetails from "./pages/ProductDetails";
 import UserProfile from "./pages/UserProfile";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<UserProfile/>} />
         <Route path="/post" element={<Post/>} />
-        <Route path="/product-details" element={<ProductDetails/>} />
       </Routes>
       <Footer />
     </>
