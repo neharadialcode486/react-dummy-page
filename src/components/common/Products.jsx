60a308102d0929aab96f5732ceb644ff42c0a038
Neha Rai <neharadialcode@gmail.com>
2025-01-17 09:56:41 +0530

    show more

--- src/components/common/Products.jsx
+++ src/components/common/Products.jsx
@@ -6,6 +6,7 @@ import CommonImg from "./CommonImg";
 
 const Products = () => {
   const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 640);
+  const [value, setValue] = useState("");
   const [visibleImages, setVisibleImages] = useState(7);
   const initialVisibleImages = 7;
   const images = [
@@ -86,39 +87,69 @@ const Products = () => {
 
   return (
     <div>
-      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 2, 750: 3, 1200: 4 }}>
-        <Masonry>
-          {isWideScreen && (
-            <CommonImg
-              imgClass="lg:h-[250px] sm:h-[150px] h-[200px] w-full object-cover rounded-xl"
-              image={boy}
-            />
-          )}
+      {value === "images" ? (
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{ 350: 2, 750: 3, 1200: 4 }}
+        >
+          <Masonry>
+            {images.slice(0, visibleImages).map((img, index) => (
+              <CommonImg
+                key={index}
+                imgClass={img.imgClass}
+                image={img.image}
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      ) : value === "content" ? (
+        <div className="max-w-[525px]">
           <MiddleImgs />
-          <span className="inline-block lg:h-[747px] sm:h-[560px] h-[500px] "></span>
-          {images.slice(0, visibleImages).map((img, index) => (
-            <CommonImg key={index} imgClass={img.imgClass} image={img.image} />
-          ))}
-        </Masonry>
-      </ResponsiveMasonry>
-      <div className="text-center">
-        {visibleImages < images.length && (
-          <button
-            onClick={handleViewMore}
-            className="mt-8 mb-4 bg-primary text-white px-6 md:px-8 py-[10px] rounded-full hover:bg-transparent border-primary border hover:text-primary transition-all duration-200 ease-in-out"
-          >
-            View More
-          </button>
-        )}
-        {visibleImages > initialVisibleImages && (
-          <button
-            onClick={handleShowLess}
-            className="mt-8 mb-4 bg-primary text-white px-6 md:px-8 py-[10px] rounded-full hover:bg-transparent border-primary border hover:text-primary transition-all duration-200 ease-in-out"
-          >
-            Show Less
-          </button>
-        )}
-      </div>
+        </div>
+      ) : (
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{ 350: 2, 750: 3, 1200: 4 }}
+        >
+          <Masonry>
+            {isWideScreen && (
+              <CommonImg
+                imgClass="lg:h-[250px] sm:h-[150px] h-[200px] w-full object-cover rounded-xl"
+                image={boy}
+              />
+            )}
+            <MiddleImgs />
+            <span className="inline-block lg:h-[747px] sm:h-[560px] h-[500px] "></span>
+            {images.slice(0, visibleImages).map((img, index) => (
+              <CommonImg
+                key={index}
+                imgClass={img.imgClass}
+                image={img.image}
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      )}
+      {value === "content" ? (
+        ""
+      ) : (
+        <div className="text-center">
+          {visibleImages < images.length && (
+            <button
+              onClick={handleViewMore}
+              className="mt-8 mb-4 bg-primary text-white px-6 md:px-8 py-[10px] rounded-full hover:bg-transparent border-primary border hover:text-primary transition-all duration-200 ease-in-out"
+            >
+              View More
+            </button>
+          )}
+          {visibleImages > initialVisibleImages && (
+            <button
+              onClick={handleShowLess}
+              className="mt-8 mb-4 bg-primary text-white px-6 md:px-8 py-[10px] rounded-full hover:bg-transparent border-primary border hover:text-primary transition-all duration-200 ease-in-out"
+            >
+              Show Less
+            </button>
+          )}
+        </div>
+      )}
     </div>
   );
 };
